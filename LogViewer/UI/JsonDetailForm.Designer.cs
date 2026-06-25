0f2cb6b70f33b77757996ad56e4d38198f4c0641
michael007js <shill@eagersoft.cn>
2026-06-25 10:13:49 +0800

    1

--- LogViewer/UI/JsonDetailForm.Designer.cs
+++ LogViewer/UI/JsonDetailForm.Designer.cs
@@ -4,76 +4,100 @@ namespace LogViewer.UI;
 
 public partial class JsonDetailForm
 {
-    private Panel _requestContainer = null!;
-    private Panel _requestToolbar = null!;
+    private System.Windows.Forms.Panel _requestContainer;
+    private System.Windows.Forms.Panel _requestToolbar;
     private Label _lblRequestTitle = null!;
-    private Panel _responseContainer = null!;
-    private Panel _responseToolbar = null!;
+    private System.Windows.Forms.Panel _responseContainer;
+    private System.Windows.Forms.Panel _responseToolbar;
     private Label _lblResponseTitle = null!;
 
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
     private void InitializeComponent()
     {
-        _split = new SplitContainer();
-        _requestContainer = new Panel();
-        _requestToolbar = new Panel();
-        _lblRequestTitle = new Label();
-        _btnToggleRequest = new Button();
-        _btnExpandReq = new Button();
-        _btnCollapseReq = new Button();
-        _btnLvl2Req = new Button();
-        _txtSearchReq = new TextBox();
-        _btnSearchReq = new Button();
-        _jsonRequest = new JsonTreeView();
-        _rawRequest = new TextBox();
-        _responseContainer = new Panel();
-        _responseToolbar = new Panel();
-        _lblResponseTitle = new Label();
-        _btnToggleResponse = new Button();
-        _btnExpandRes = new Button();
-        _btnCollapseRes = new Button();
-        _btnLvl2Res = new Button();
-        _txtSearchRes = new TextBox();
-        _btnSearchRes = new Button();
-        _jsonResponse = new JsonTreeView();
-        _rawResponse = new TextBox();
-
-        SuspendLayout();
+        _split = new System.Windows.Forms.SplitContainer();
+        _requestContainer = new System.Windows.Forms.Panel();
+        _rawRequest = new System.Windows.Forms.TextBox();
+        _jsonRequest = new LogViewer.UI.JsonTreeView();
+        _requestToolbar = new System.Windows.Forms.Panel();
+        _lblRequestTitle = new System.Windows.Forms.Label();
+        _btnToggleRequest = new System.Windows.Forms.Button();
+        _btnExpandReq = new System.Windows.Forms.Button();
+        _btnCollapseReq = new System.Windows.Forms.Button();
+        _btnLvl2Req = new System.Windows.Forms.Button();
+        _txtSearchReq = new System.Windows.Forms.TextBox();
+        _btnSearchReq = new System.Windows.Forms.Button();
+        _responseContainer = new System.Windows.Forms.Panel();
+        _rawResponse = new System.Windows.Forms.TextBox();
+        _jsonResponse = new LogViewer.UI.JsonTreeView();
+        _responseToolbar = new System.Windows.Forms.Panel();
+        _lblResponseTitle = new System.Windows.Forms.Label();
+        _btnToggleResponse = new System.Windows.Forms.Button();
+        _btnExpandRes = new System.Windows.Forms.Button();
+        _btnCollapseRes = new System.Windows.Forms.Button();
+        _btnLvl2Res = new System.Windows.Forms.Button();
+        _txtSearchRes = new System.Windows.Forms.TextBox();
+        _btnSearchRes = new System.Windows.Forms.Button();
         ((System.ComponentModel.ISupportInitialize)_split).BeginInit();
         _split.Panel1.SuspendLayout();
         _split.Panel2.SuspendLayout();
         _split.SuspendLayout();
-
-        Text = string.Format(Language.JsonDetailTitle, _entry.Method ?? string.Empty, _entry.UrlPath, _entry.Code, _entry.Duration);
-        Size = new Size(1000, 600);
-        MinimumSize = new Size(600, 400);
-        StartPosition = FormStartPosition.CenterParent;
-        Font = new Font("Consolas", 9F);
-        KeyPreview = true;
-
-        _split.Dock = DockStyle.Fill;
-        _split.Orientation = Orientation.Vertical;
+        _requestContainer.SuspendLayout();
+        _requestToolbar.SuspendLayout();
+        _responseContainer.SuspendLayout();
+        _responseToolbar.SuspendLayout();
+        SuspendLayout();
+        // 
+        // _split
+        // 
+        _split.Dock = System.Windows.Forms.DockStyle.Fill;
+        _split.Location = new System.Drawing.Point(0, 0);
+        _split.Name = "_split";
+        // 
+        // _split.Panel1
+        // 
+        _split.Panel1.Controls.Add(_requestContainer);
         _split.Panel1MinSize = 150;
-        _split.SplitterWidth = 4;
-
-        _requestContainer.Dock = DockStyle.Fill;
-        _requestToolbar.Dock = DockStyle.Top;
-        _requestToolbar.Height = 26;
-        _lblRequestTitle.Text = Language.RequestBody;
-        _lblRequestTitle.Location = new Point(0, 4);
-        _lblRequestTitle.AutoSize = true;
-        _lblRequestTitle.Font = new Font("Consolas", 9F, FontStyle.Bold);
-        _btnToggleRequest.Text = Language.Raw;
-        _btnToggleRequest.SetBounds(100, 2, 42, 23);
-        _btnExpandReq.Text = Language.Expand;
-        _btnExpandReq.SetBounds(148, 2, 50, 23);
-        _btnCollapseReq.Text = Language.Collapse;
-        _btnCollapseReq.SetBounds(200, 2, 55, 23);
-        _btnLvl2Req.Text = Language.CollapseLevel2;
-        _btnLvl2Req.SetBounds(257, 2, 36, 23);
-        _txtSearchReq.SetBounds(300, 3, 80, 23);
-        _txtSearchReq.PlaceholderText = Language.SearchPlaceholder;
-        _btnSearchReq.Text = "\u25B6";
-        _btnSearchReq.SetBounds(382, 2, 22, 23);
+        // 
+        // _split.Panel2
+        // 
+        _split.Panel2.Controls.Add(_responseContainer);
+        _split.Size = new System.Drawing.Size(984, 561);
+        _split.SplitterDistance = 483;
+        _split.TabIndex = 0;
+        // 
+        // _requestContainer
+        // 
+        _requestContainer.Controls.Add(_rawRequest);
+        _requestContainer.Controls.Add(_jsonRequest);
+        _requestContainer.Controls.Add(_requestToolbar);
+        _requestContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+        _requestContainer.Location = new System.Drawing.Point(0, 0);
+        _requestContainer.Name = "_requestContainer";
+        _requestContainer.Size = new System.Drawing.Size(483, 561);
+        _requestContainer.TabIndex = 0;
+        // 
+        // _rawRequest
+        // 
+        _rawRequest.Location = new System.Drawing.Point(0, 0);
+        _rawRequest.Name = "_rawRequest";
+        _rawRequest.Size = new System.Drawing.Size(100, 22);
+        _rawRequest.TabIndex = 0;
+        // 
+        // _jsonRequest
+        // 
+        _jsonRequest.BackColor = System.Drawing.SystemColors.Window;
+        _jsonRequest.Dock = System.Windows.Forms.DockStyle.Fill;
+        _jsonRequest.Font = new System.Drawing.Font("Consolas", 11F);
+        _jsonRequest.Location = new System.Drawing.Point(0, 26);
+        _jsonRequest.Name = "_jsonRequest";
+        _jsonRequest.Size = new System.Drawing.Size(483, 535);
+        _jsonRequest.TabIndex = 1;
+        // 
+        // _requestToolbar
+        // 
         _requestToolbar.Controls.Add(_lblRequestTitle);
         _requestToolbar.Controls.Add(_btnToggleRequest);
         _requestToolbar.Controls.Add(_btnExpandReq);
@@ -81,32 +105,100 @@ public partial class JsonDetailForm
         _requestToolbar.Controls.Add(_btnLvl2Req);
         _requestToolbar.Controls.Add(_txtSearchReq);
         _requestToolbar.Controls.Add(_btnSearchReq);
-        _jsonRequest.Dock = DockStyle.Fill;
-        ConfigureRawTextBox(_rawRequest);
-        _requestContainer.Controls.Add(_rawRequest);
-        _requestContainer.Controls.Add(_jsonRequest);
-        _requestContainer.Controls.Add(_requestToolbar);
-        _split.Panel1.Controls.Add(_requestContainer);
-
-        _responseContainer.Dock = DockStyle.Fill;
-        _responseToolbar.Dock = DockStyle.Top;
-        _responseToolbar.Height = 26;
-        _lblResponseTitle.Text = Language.ResponseBody;
-        _lblResponseTitle.Location = new Point(0, 4);
-        _lblResponseTitle.AutoSize = true;
-        _lblResponseTitle.Font = new Font("Consolas", 9F, FontStyle.Bold);
-        _btnToggleResponse.Text = Language.Raw;
-        _btnToggleResponse.SetBounds(110, 2, 42, 23);
-        _btnExpandRes.Text = Language.Expand;
-        _btnExpandRes.SetBounds(158, 2, 50, 23);
-        _btnCollapseRes.Text = Language.Collapse;
-        _btnCollapseRes.SetBounds(210, 2, 55, 23);
-        _btnLvl2Res.Text = Language.CollapseLevel2;
-        _btnLvl2Res.SetBounds(267, 2, 36, 23);
-        _txtSearchRes.SetBounds(310, 3, 80, 23);
-        _txtSearchRes.PlaceholderText = Language.SearchPlaceholder;
-        _btnSearchRes.Text = "\u25B6";
-        _btnSearchRes.SetBounds(392, 2, 22, 23);
+        _requestToolbar.Dock = System.Windows.Forms.DockStyle.Top;
+        _requestToolbar.Location = new System.Drawing.Point(0, 0);
+        _requestToolbar.Name = "_requestToolbar";
+        _requestToolbar.Size = new System.Drawing.Size(483, 26);
+        _requestToolbar.TabIndex = 2;
+        // 
+        // _lblRequestTitle
+        // 
+        _lblRequestTitle.AutoSize = true;
+        _lblRequestTitle.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold);
+        _lblRequestTitle.Location = new System.Drawing.Point(0, 4);
+        _lblRequestTitle.Name = "_lblRequestTitle";
+        _lblRequestTitle.Size = new System.Drawing.Size(52, 14);
+        _lblRequestTitle.TabIndex = 0;
+        _lblRequestTitle.Text = "请求体";
+        // 
+        // _btnToggleRequest
+        // 
+        _btnToggleRequest.Location = new System.Drawing.Point(100, 2);
+        _btnToggleRequest.Name = "_btnToggleRequest";
+        _btnToggleRequest.Size = new System.Drawing.Size(42, 23);
+        _btnToggleRequest.TabIndex = 1;
+        _btnToggleRequest.Text = "原文";
+        // 
+        // _btnExpandReq
+        // 
+        _btnExpandReq.Location = new System.Drawing.Point(148, 2);
+        _btnExpandReq.Name = "_btnExpandReq";
+        _btnExpandReq.Size = new System.Drawing.Size(50, 23);
+        _btnExpandReq.TabIndex = 2;
+        _btnExpandReq.Text = "展开";
+        // 
+        // _btnCollapseReq
+        // 
+        _btnCollapseReq.Location = new System.Drawing.Point(200, 2);
+        _btnCollapseReq.Name = "_btnCollapseReq";
+        _btnCollapseReq.Size = new System.Drawing.Size(55, 23);
+        _btnCollapseReq.TabIndex = 3;
+        _btnCollapseReq.Text = "折叠";
+        // 
+        // _btnLvl2Req
+        // 
+        _btnLvl2Req.Location = new System.Drawing.Point(257, 2);
+        _btnLvl2Req.Name = "_btnLvl2Req";
+        _btnLvl2Req.Size = new System.Drawing.Size(56, 23);
+        _btnLvl2Req.TabIndex = 4;
+        _btnLvl2Req.Text = "二级";
+        // 
+        // _txtSearchReq
+        // 
+        _txtSearchReq.Location = new System.Drawing.Point(320, 3);
+        _txtSearchReq.Name = "_txtSearchReq";
+        _txtSearchReq.PlaceholderText = "搜索...";
+        _txtSearchReq.Size = new System.Drawing.Size(80, 22);
+        _txtSearchReq.TabIndex = 5;
+        // 
+        // _btnSearchReq
+        // 
+        _btnSearchReq.Location = new System.Drawing.Point(402, 2);
+        _btnSearchReq.Name = "_btnSearchReq";
+        _btnSearchReq.Size = new System.Drawing.Size(22, 23);
+        _btnSearchReq.TabIndex = 6;
+        _btnSearchReq.Text = "▶";
+        // 
+        // _responseContainer
+        // 
+        _responseContainer.Controls.Add(_rawResponse);
+        _responseContainer.Controls.Add(_jsonResponse);
+        _responseContainer.Controls.Add(_responseToolbar);
+        _responseContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+        _responseContainer.Location = new System.Drawing.Point(0, 0);
+        _responseContainer.Name = "_responseContainer";
+        _responseContainer.Size = new System.Drawing.Size(497, 561);
+        _responseContainer.TabIndex = 0;
+        // 
+        // _rawResponse
+        // 
+        _rawResponse.Location = new System.Drawing.Point(0, 0);
+        _rawResponse.Name = "_rawResponse";
+        _rawResponse.Size = new System.Drawing.Size(100, 22);
+        _rawResponse.TabIndex = 0;
+        // 
+        // _jsonResponse
+        // 
+        _jsonResponse.BackColor = System.Drawing.SystemColors.Window;
+        _jsonResponse.Dock = System.Windows.Forms.DockStyle.Fill;
+        _jsonResponse.Font = new System.Drawing.Font("Consolas", 11F);
+        _jsonResponse.Location = new System.Drawing.Point(0, 26);
+        _jsonResponse.Name = "_jsonResponse";
+        _jsonResponse.Size = new System.Drawing.Size(497, 535);
+        _jsonResponse.TabIndex = 1;
+        // 
+        // _responseToolbar
+        // 
         _responseToolbar.Controls.Add(_lblResponseTitle);
         _responseToolbar.Controls.Add(_btnToggleResponse);
         _responseToolbar.Controls.Add(_btnExpandRes);
@@ -114,19 +206,90 @@ public partial class JsonDetailForm
         _responseToolbar.Controls.Add(_btnLvl2Res);
         _responseToolbar.Controls.Add(_txtSearchRes);
         _responseToolbar.Controls.Add(_btnSearchRes);
-        _jsonResponse.Dock = DockStyle.Fill;
-        ConfigureRawTextBox(_rawResponse);
-        _responseContainer.Controls.Add(_rawResponse);
-        _responseContainer.Controls.Add(_jsonResponse);
-        _responseContainer.Controls.Add(_responseToolbar);
-        _split.Panel2.Controls.Add(_responseContainer);
-
+        _responseToolbar.Dock = System.Windows.Forms.DockStyle.Top;
+        _responseToolbar.Location = new System.Drawing.Point(0, 0);
+        _responseToolbar.Name = "_responseToolbar";
+        _responseToolbar.Size = new System.Drawing.Size(497, 26);
+        _responseToolbar.TabIndex = 2;
+        // 
+        // _lblResponseTitle
+        // 
+        _lblResponseTitle.AutoSize = true;
+        _lblResponseTitle.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold);
+        _lblResponseTitle.Location = new System.Drawing.Point(0, 4);
+        _lblResponseTitle.Name = "_lblResponseTitle";
+        _lblResponseTitle.Size = new System.Drawing.Size(52, 14);
+        _lblResponseTitle.TabIndex = 0;
+        _lblResponseTitle.Text = "响应体";
+        // 
+        // _btnToggleResponse
+        // 
+        _btnToggleResponse.Location = new System.Drawing.Point(110, 2);
+        _btnToggleResponse.Name = "_btnToggleResponse";
+        _btnToggleResponse.Size = new System.Drawing.Size(42, 23);
+        _btnToggleResponse.TabIndex = 1;
+        _btnToggleResponse.Text = "原文";
+        // 
+        // _btnExpandRes
+        // 
+        _btnExpandRes.Location = new System.Drawing.Point(158, 2);
+        _btnExpandRes.Name = "_btnExpandRes";
+        _btnExpandRes.Size = new System.Drawing.Size(50, 23);
+        _btnExpandRes.TabIndex = 2;
+        _btnExpandRes.Text = "展开";
+        // 
+        // _btnCollapseRes
+        // 
+        _btnCollapseRes.Location = new System.Drawing.Point(210, 2);
+        _btnCollapseRes.Name = "_btnCollapseRes";
+        _btnCollapseRes.Size = new System.Drawing.Size(55, 23);
+        _btnCollapseRes.TabIndex = 3;
+        _btnCollapseRes.Text = "折叠";
+        // 
+        // _btnLvl2Res
+        // 
+        _btnLvl2Res.Location = new System.Drawing.Point(267, 2);
+        _btnLvl2Res.Name = "_btnLvl2Res";
+        _btnLvl2Res.Size = new System.Drawing.Size(56, 23);
+        _btnLvl2Res.TabIndex = 4;
+        _btnLvl2Res.Text = "二级";
+        // 
+        // _txtSearchRes
+        // 
+        _txtSearchRes.Location = new System.Drawing.Point(320, 3);
+        _txtSearchRes.Name = "_txtSearchRes";
+        _txtSearchRes.PlaceholderText = "搜索...";
+        _txtSearchRes.Size = new System.Drawing.Size(80, 22);
+        _txtSearchRes.TabIndex = 5;
+        // 
+        // _btnSearchRes
+        // 
+        _btnSearchRes.Location = new System.Drawing.Point(402, 2);
+        _btnSearchRes.Name = "_btnSearchRes";
+        _btnSearchRes.Size = new System.Drawing.Size(22, 23);
+        _btnSearchRes.TabIndex = 6;
+        _btnSearchRes.Text = "▶";
+        // 
+        // JsonDetailForm
+        // 
+        ClientSize = new System.Drawing.Size(984, 561);
         Controls.Add(_split);
-
-        _split.Panel2.ResumeLayout(false);
+        Font = new System.Drawing.Font("Consolas", 9F);
+        KeyPreview = true;
+        MinimumSize = new System.Drawing.Size(600, 400);
+        StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
         _split.Panel1.ResumeLayout(false);
+        _split.Panel2.ResumeLayout(false);
         ((System.ComponentModel.ISupportInitialize)_split).EndInit();
         _split.ResumeLayout(false);
+        _requestContainer.ResumeLayout(false);
+        _requestContainer.PerformLayout();
+        _requestToolbar.ResumeLayout(false);
+        _requestToolbar.PerformLayout();
+        _responseContainer.ResumeLayout(false);
+        _responseContainer.PerformLayout();
+        _responseToolbar.ResumeLayout(false);
+        _responseToolbar.PerformLayout();
         ResumeLayout(false);
     }
 
--- LogViewer/UI/JsonDetailForm.cs
+++ LogViewer/UI/JsonDetailForm.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel;
-using System.Diagnostics;
 using LogViewer.Models;
 using LogViewer.Static;
 using LogViewer.Utils;
@@ -13,19 +11,15 @@ namespace LogViewer.UI;
 public partial class JsonDetailForm : Form
 {
     /// <summary>当前显示的日志条目。</summary>
-    private readonly LogEntry _entry;
+    private LogEntry _entry = new();
 
-    /// <summary>字体设置。</summary>
-    private readonly Font _font;
+    private Font _font = SystemFonts.DefaultFont;
 
-    /// <summary>左右分栏容器。</summary>
-    private SplitContainer _split = null!;
+    private SplitContainer _split;
 
-    /// <summary>请求 JSON 树视图。</summary>
-    private JsonTreeView _jsonRequest = null!;
+    private JsonTreeView _jsonRequest;
 
-    /// <summary>响应 JSON 树视图。</summary>
-    private JsonTreeView _jsonResponse = null!;
+    private JsonTreeView _jsonResponse;
 
     /// <summary>请求原始文本框。</summary>
     private TextBox _rawRequest = null!;
@@ -45,14 +39,11 @@ public partial class JsonDetailForm : Form
     /// <summary>折叠请求 JSON 树的按钮。</summary>
     private Button _btnCollapseReq = null!;
 
-    /// <summary>折叠请求 JSON 树到第2级的按钮。</summary>
-    private Button _btnLvl2Req = null!;
+    private Button _btnLvl2Req;
 
-    /// <summary>请求搜索框。</summary>
-    private TextBox _txtSearchReq = null!;
+    private TextBox _txtSearchReq;
 
-    /// <summary>请求搜索按钮。</summary>
-    private Button _btnSearchReq = null!;
+    private Button _btnSearchReq;
 
     /// <summary>展开响应 JSON 树的按钮。</summary>
     private Button _btnExpandRes = null!;
@@ -60,14 +51,11 @@ public partial class JsonDetailForm : Form
     /// <summary>折叠响应 JSON 树的按钮。</summary>
     private Button _btnCollapseRes = null!;
 
-    /// <summary>折叠响应 JSON 树到第2级的按钮。</summary>
-    private Button _btnLvl2Res = null!;
+    private Button _btnLvl2Res;
 
-    /// <summary>响应搜索框。</summary>
-    private TextBox _txtSearchRes = null!;
+    private TextBox _txtSearchRes;
 
-    /// <summary>响应搜索按钮。</summary>
-    private Button _btnSearchRes = null!;
+    private Button _btnSearchRes;
 
     /// <summary>请求视图是否为原始文本模式。</summary>
     private bool _requestIsRaw;
@@ -78,43 +66,22 @@ public partial class JsonDetailForm : Form
     /// <summary>
     /// 设计器模式构造函数。
     /// </summary>
-    public JsonDetailForm() : this(new LogEntry(), SystemFonts.DefaultFont, true)
+    public JsonDetailForm()
     {
+        InitializeComponent();
     }
 
-    /// <summary>
-    /// 初始化 JSON 详情窗口。
-    /// </summary>
-    /// <param name="entry">要显示的日志条目。</param>
-    /// <param name="font">字体设置。</param>
     public JsonDetailForm(LogEntry entry, Font font)
-        : this(entry, font, false)
-    {
-    }
-
-    /// <summary>
-    /// 私有构造函数，处理初始化逻辑。
-    /// </summary>
-    private JsonDetailForm(LogEntry entry, Font font, bool designMode)
     {
         _entry = entry;
         _font = new Font(font.FontFamily, font.Size);
         InitializeComponent();
-        if (designMode || IsDesignTimeMode())
-        {
-            return;
-        }
-
+        Text = string.Format(Language.JsonDetailTitle, _entry.Method ?? string.Empty, _entry.UrlPath ?? string.Empty,
+            _entry.Code, _entry.Duration);
         WireComponentEvents();
-        if (!designMode)
-        {
-            LoadData();
-        }
+        LoadData();
     }
 
-    /// <summary>
-    /// 连接组件事件。
-    /// </summary>
     private void WireComponentEvents()
     {
         _btnToggleRequest.Click += (s, e) => ToggleRequestView();
@@ -130,9 +97,6 @@ public partial class JsonDetailForm : Form
         Shown += (s, e) => BeginInvoke(new Action(ApplyInitialSplitterLayout));
     }
 
-    /// <summary>
-    /// 应用初始分栏布局，设置左右面板比例为 4:6。
-    /// </summary>
     private void ApplyInitialSplitterLayout()
     {
         int total = _split.ClientSize.Width - _split.SplitterWidth;
@@ -149,33 +113,6 @@ public partial class JsonDetailForm : Form
         _split.SplitterDistance = distance;
     }
 
-    /// <summary>
-    /// 检测当前是否处于 IDE 设计器模式，防止设计器中触发运行时逻辑导致崩溃。
-    /// 同时检查进程名和命令行参数以覆盖 Visual Studio / Rider / JetBrains 设计器场景。
-    /// </summary>
-    /// <returns>处于设计器模式返回 true。</returns>
-    private static bool IsDesignTimeMode()
-    {
-        if (LicenseManager.UsageMode == LicenseUsageMode.Designtime)
-        {
-            return true;
-        }
-
-        var processName = Process.GetCurrentProcess().ProcessName;
-        var commandLine = Environment.CommandLine;
-        return processName.Contains("devenv", StringComparison.OrdinalIgnoreCase) ||
-               processName.Contains("DesignToolsServer", StringComparison.OrdinalIgnoreCase) ||
-               processName.Contains("rider", StringComparison.OrdinalIgnoreCase) ||
-               processName.Contains("jetbrains", StringComparison.OrdinalIgnoreCase) ||
-               commandLine.Contains("JetBrains.ReSharper.Features.WinForms.Designer.External.Core",
-                   StringComparison.OrdinalIgnoreCase) ||
-               commandLine.Contains("WinFormsDesigner", StringComparison.OrdinalIgnoreCase);
-    }
-
-    /// <summary>
-    /// 加载日志数据到请求和响应的 JSON 树视图及原始文本框。
-    /// 对 JSON 内容尝试格式化，格式化失败则显示原始文本。
-    /// </summary>
     private void LoadData()
     {
         _jsonRequest.DisplayJson(_entry.Send ?? "");
@@ -184,10 +121,6 @@ public partial class JsonDetailForm : Form
         _rawResponse.Text = JsonFormatter.FormatJson(_entry.Content) ?? _entry.Content ?? "";
     }
 
-    /// <summary>
-    /// 切换请求视图模式：JSON 树视图 ↔ 原始文本。
-    /// 切换时同步启用/禁用树操作按钮和搜索控件。
-    /// </summary>
     private void ToggleRequestView()
     {
         _requestIsRaw = !_requestIsRaw;
@@ -201,10 +134,6 @@ public partial class JsonDetailForm : Form
         _btnSearchReq.Enabled = !_requestIsRaw;
     }
 
-    /// <summary>
-    /// 切换响应视图模式：JSON 树视图 ↔ 原始文本。
-    /// 切换时同步启用/禁用树操作按钮和搜索控件。
-    /// </summary>
     private void ToggleResponseView()
     {
         _responseIsRaw = !_responseIsRaw;
@@ -218,12 +147,6 @@ public partial class JsonDetailForm : Form
         _btnSearchRes.Enabled = !_responseIsRaw;
     }
 
-    /// <summary>
-    /// 处理快捷键：Ctrl+W 关闭窗口。
-    /// </summary>
-    /// <param name="msg">Windows 消息。</param>
-    /// <param name="keyData">按键组合。</param>
-    /// <returns>已处理返回 true；否则调用基类处理。</returns>
     protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
     {
         if (keyData == (Keys.Control | Keys.W))
@@ -235,10 +158,6 @@ public partial class JsonDetailForm : Form
         return base.ProcessCmdKey(ref msg, keyData);
     }
 
-    /// <summary>
-    /// 释放资源，清理自定义字体实例。
-    /// </summary>
-    /// <param name="disposing">是否释放托管资源。</param>
     protected override void Dispose(bool disposing)
     {
         if (disposing) _font?.Dispose();
